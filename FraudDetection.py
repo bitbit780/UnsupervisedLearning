93841bc806069a49e5d71113bf6c912046517747
hiroki_desktop <bitbit780@icloud.com>
2026-04-25 21:30:24 +0900

    Step2.3.2

---
+++ FraudDetection.py
@@ -0,0 +1,38 @@
+# Main
+import numpy as np
+import pandas as pd
+import os
+
+# Data Viz
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+import matplotlib as mpl
+
+# Data prep
+from sklearn import preprocessing as pp
+from scipy.stats import pearsonr
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import log_loss
+from sklearn.metrics import precision_recall_curve, average_precision_score
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+from sklearn.metrics import confusion_matrix, classification_report
+
+# Algos
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
+import lightgbm as lgb
+
+current_path = os.getcwd()
+file = os.path.sep.join(['', 'datasets', 'credit_card_data', 'credit_card.csv'])
+data = pd.read_csv(current_path + file)
+data.head()
+print(data.describe())
+print(data.columns)
+print("Number of fraudulent transactions:", data['Class'].sum())
+nonCounter = np.isnan(data).sum()
+print(nonCounter)
+distinctCounter = data.apply(lambda x: len(x.unique()))
+print(distinctCounter)

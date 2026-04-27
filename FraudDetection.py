e5feccc779f1a76110a19ccd29892067eb5f8275
hiroki_laptop <bitbit780@icloud.com>
2026-04-27 14:30:22 +0900

    Step2.5.1

--- FraudDetection.py
+++ FraudDetection.py
@@ -28,14 +28,14 @@ import lightgbm as lgb
 current_path = os.getcwd()
 file = os.path.sep.join(['', 'datasets', 'credit_card_data', 'credit_card.csv'])
 data = pd.read_csv(current_path + file)
-data.head()
-print(data.describe())
-print(data.columns)
-print("Number of fraudulent transactions:", data['Class'].sum())
-nonCounter = np.isnan(data).sum()
-print(nonCounter)
-distinctCounter = data.apply(lambda x: len(x.unique()))
-print(distinctCounter)
+# data.head()
+# print(data.describe())
+# print(data.columns)
+# print("Number of fraudulent transactions:", data['Class'].sum())
+# nonCounter = np.isnan(data).sum()
+# print(nonCounter)
+# distinctCounter = data.apply(lambda x: len(x.unique()))
+# print(distinctCounter)
 
 dataX = data.copy().drop(['Class'], axis=1)
 dataY = data['Class'].copy()
@@ -44,11 +44,46 @@ sX = pp.StandardScaler(copy=True)
 dataX.loc[:,featuresToScale] = sX.fit_transform(dataX[featuresToScale])
 print(dataX.describe())
 
-count_classes = data['Class'].value_counts(sort=True).sort_index()
-relative_freq = count_classes / len(data)
-plt.figure(figsize=(6,4))
-ax = sns.barplot(x=relative_freq.index, y=relative_freq.values)
-ax.set_title('Frequency Percentage by Class')
-ax.set_xlabel('Class')
-ax.set_ylabel('Frequency Percentage')
-plt.show()
+# count_classes = data['Class'].value_counts(sort=True).sort_index()
+# relative_freq = count_classes / len(data)
+# plt.figure(figsize=(6,4))
+# ax = sns.barplot(x=relative_freq.index, y=relative_freq.values)
+# ax.set_title('Frequency Percentage by Class')
+# ax.set_xlabel('Class')
+# ax.set_ylabel('Frequency Percentage')
+# plt.show()
+
+# 訓練セットとテストセットに分割
+x_train, x_test, y_train, y_test = train_test_split(dataX, dataY, test_size=0.33, random_state=2018, stratify=dataY)
+k_fold = StratifiedKFold(n_splits=5, shuffle=True, random_state=2018)
+
+# ロジスティック回帰
+## ハイパーパラメータの設定
+penalty = 'l2'
+C = 1.0
+class_weight = 'balanced'
+random_state = 2018
+solover = 'liblinear'
+n_jobs = 1
+logReg = LogisticRegression(penalty=penalty, C=C, class_weight=class_weight, random_state=random_state, solver=solover, n_jobs=n_jobs)
+
+## モデルの訓練
+trainingScores = []
+cvScores = []
+predictionsBaseOnKFolds = pd.DataFrame(data=[], index=y_train.index, columns=[0,1])
+model = logReg
+
+for train_index, cv_index in k_fold.split(np.zeros(len(x_train)), y_train.values.ravel()):
+    x_train_fold, x_cv_fold = x_train.iloc[train_index,:], x_train.iloc[cv_index,:]
+    y_train_fold, y_cv_fold = y_train.iloc[train_index], y_train.iloc[cv_index]
+    model.fit(x_train_fold, y_train_fold)
+    loglossTraining = log_loss(y_train_fold, model.predict_proba(x_train_fold)[:,1])
+    trainingScores.append(loglossTraining)
+    predictionsBaseOnKFolds.loc[x_cv_fold.index,:] = model.predict_proba(x_cv_fold)
+    loglossCV = log_loss(y_cv_fold, predictionsBaseOnKFolds.loc[x_cv_fold.index,1])
+    cvScores.append(loglossCV)
+    print('Training Log Loss: ', loglossTraining)
+    print('CV Log Loss: ', loglossCV)
+
+loglossLogisticRegression = log_loss(y_train, predictionsBaseOnKFolds.loc[:,1])
+print('Logistic Regression Log Loss:', loglossLogisticRegression)
